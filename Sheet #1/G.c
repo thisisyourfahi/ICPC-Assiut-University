65c0458ab2fb7173a7e7725f58b217c367cc9a51
Fahiyan <fahiyanofficial@gmail.com>
2024-10-08 00:19:22 +0600

    fixed

--- Sheet #1/G.c
+++ Sheet #1/G.c
@@ -3,11 +3,11 @@ int main(){
     int n;
     scanf("%d", &n);
 
-    int sum = 0;
+    int totalsum = 0;
     for(int i = 1; i <= n; i++){
-        sum += i;
+        totalsum += i;
     }
-    printf("%d\n", sum);
+    printf("%d\n", totalsum);
 
     return 0;
 }

bf180f54e3b742f52458d0929d8e98cdd6a83b87
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-10-10 22:41:45 +0600

    Create C.c

---
+++ Sheet #3/C.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] > 0){
+            arr[i] = 1;
+        }
+        else if(arr[i] < 0){
+            arr[i] = 2;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}

666af506dae46c04f61ddd45b081b3c03c9e4e2a
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-10-27 00:19:10 +0600

    Bubble Sort

---
+++ Sheet #3/H.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for(int j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped){
+            break;
+        }
+    }
+    
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}

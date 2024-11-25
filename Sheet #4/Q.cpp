98609963d17253858f68d4e5e39cb53f7354d2a6
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-11-25 20:11:40 +0600

    Create Q.cpp

---
+++ Sheet #4/Q.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    
+    stringstream ss(s);
+    string word;
+    ss >> word;
+    reverse(word.begin(), word.end());
+    cout << word;
+    while(ss >> word) {
+        reverse(word.begin(), word.end());
+        cout << " " << word;
+    }
+    cout << endl;
+
+    return 0;
+}

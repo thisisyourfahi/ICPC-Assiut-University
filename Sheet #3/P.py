05be1e0b57f9059406fdceb844677f6519ebb39d
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2025-09-28 12:28:00 +0600

    Update P.py

--- Sheet #3/P.py
+++ Sheet #3/P.py
@@ -14,3 +14,14 @@ while True:
     cnt += 1
 
 print(cnt)
+
+
+# even simpler version
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# cnt = 0
+# while all(x % 2 == 0 for x in a):
+#     a= [x // 2 for x in a]
+#     cnt += 1
+# print(cnt)
